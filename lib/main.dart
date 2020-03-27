05350fa39f8134a996512977d37e0f6d084cd83c
“Mitul <mitul.patel@tristatetechnology.co.in>
2020-03-27 15:51:02 +0530

    init commit for bitrise

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+
         title: Text("Home Page"),
       ),
       body: Center(
