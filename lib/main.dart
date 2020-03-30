f77ecc1acae8d2c7b7a02a2251b06a28ed688344
“Mitul <mitul.patel@tristatetechnology.co.in>
2020-03-30 12:03:22 +0530

    after removing folders

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text("PMI Flutter"),
       ),
       body: Center(
